8830bfccc7f50aa439c387fb1d70bdaa365953a9
JessicaGalhardi <104734490+JessicaGalhardi@users.noreply.github.com>
2022-11-02 19:00:02 +0100

    Adicionar arquivos de projeto.

---
+++ ADS_FUNDAMENTOSCSHARPE_01112022_LOGICA_NEGAÇÃO/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Negacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Valores apresentados no diagnóstico de um paciente.
+
+                string sintomasGripais = "positivo";
+                int temperaturaCorporal = 40;
+                string testeAntigeno = "positivo";
+
+                //_________________________________________________________________________________________
+                //Expressões lógicas para definir se um paciente deve ou não fazer quarentena!
+                
+                if(!(testeAntigeno=="negativo") && !(sintomasGripais== "negativo") && !(temperaturaCorporal<=37.8))
+                {
+                    Console.WriteLine("Este paciente precisa fazer 30 dias de quarentena!");
+                }
+                else
+                {
+                    Console.WriteLine("Este paciente não precisa quarentena!");
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao executar a aplicação!" + ex);//Em caso de erro é exibida a mensagem junto com o erro apresentado.
+            }
+        }
+    }
+
+}
